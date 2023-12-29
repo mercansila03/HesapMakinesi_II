eeb7e1f8ab2bed39f6e4a836bef82e28cac9f2a6
SılaMercan <120477952+mercansila03@users.noreply.github.com>
2023-12-29 16:46:08 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Hesap_Makinesi_II
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.sayi2 = new System.Windows.Forms.TextBox();
+            this.sayi1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnSonuc = new System.Windows.Forms.Button();
+            this.labelSonuc = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(57, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1.Sayı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(472, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "2.Sayı";
+            // 
+            // sayi2
+            // 
+            this.sayi2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sayi2.Location = new System.Drawing.Point(545, 56);
+            this.sayi2.Name = "sayi2";
+            this.sayi2.Size = new System.Drawing.Size(100, 30);
+            this.sayi2.TabIndex = 2;
+            // 
+            // sayi1
+            // 
+            this.sayi1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sayi1.Location = new System.Drawing.Point(130, 54);
+            this.sayi1.Name = "sayi1";
+            this.sayi1.Size = new System.Drawing.Size(100, 30);
+            this.sayi1.TabIndex = 3;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "+(toplama)",
+            "-(çıkarma)",
+            "x(çarpma)",
+            "/(bölme)"});
+            this.comboBox1.Location = new System.Drawing.Point(264, 51);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(180, 33);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.Text = "İşlemi Seçiniz";
+            // 
+            // btnSonuc
+            // 
+            this.btnSonuc.BackColor = System.Drawing.Color.Transparent;
+            this.btnSonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSonuc.Location = new System.Drawing.Point(264, 167);
+            this.btnSonuc.Name = "btnSonuc";
+            this.btnSonuc.Size = new System.Drawing.Size(180, 69);
+            this.btnSonuc.TabIndex = 5;
+            this.btnSonuc.Text = "Sonuç";
+            this.btnSonuc.UseVisualStyleBackColor = false;
+            this.btnSonuc.Click += new System.EventHandler(this.btnSonuc_Click);
+            // 
+            // labelSonuc
+            // 
+            this.labelSonuc.AutoSize = true;
+            this.labelSonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelSonuc.Location = new System.Drawing.Point(12, 12);
+            this.labelSonuc.Name = "labelSonuc";
+            this.labelSonuc.Size = new System.Drawing.Size(0, 25);
+            this.labelSonuc.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(264, 277);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 69);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Çıkış";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.labelSonuc);
+            this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.panel1.Location = new System.Drawing.Point(488, 177);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(98, 48);
+            this.panel1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Hesap_Makinesi_II.Properties.Resources.hesapmakinesiwp;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(809, 427);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnSonuc);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.sayi1);
+            this.Controls.Add(this.sayi2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hesap Makinesi";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox sayi2;
+        private System.Windows.Forms.TextBox sayi1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnSonuc;
+        private System.Windows.Forms.Label labelSonuc;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hesap_Makinesi_II
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+      
+
+    
+       
+
+        private void btnSonuc_Click(object sender, EventArgs e)
+        {
+            if(comboBox1.SelectedIndex==0)
+            {
+                double toplam=Convert.ToDouble(sayi1.Text) + Convert.ToDouble(sayi2.Text);
+                labelSonuc.Text=Convert.ToString(toplam);
+            }else
+
+            if (comboBox1.SelectedIndex == 1)
+            {
+                double fark = Convert.ToDouble(sayi1.Text) - Convert.ToDouble(sayi2.Text);
+                labelSonuc.Text = Convert.ToString(fark);
+            }else
+
+            if (comboBox1.SelectedIndex == 2)
+            {
+                double carpim = Convert.ToDouble(sayi1.Text) * Convert.ToDouble(sayi2.Text);
+                labelSonuc.Text = Convert.ToString(carpim);
+            }else
+
+            if (comboBox1.SelectedIndex == 3)
+            {
+                double bolme = Convert.ToDouble(sayi1.Text) / Convert.ToDouble(sayi2.Text);
+                labelSonuc.Text = Convert.ToString(bolme);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
